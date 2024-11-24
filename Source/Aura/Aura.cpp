6b772b17f00a3224a8d3b9a802c5ad3073470f5a
Joel Keeling <7408775+joelkeeling@users.noreply.github.com>
2024-11-23 19:23:08 -0700

    Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Joel Keeling
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Joel Keeling
+
+#pragma once
+
+#include "CoreMinimal.h"
+
