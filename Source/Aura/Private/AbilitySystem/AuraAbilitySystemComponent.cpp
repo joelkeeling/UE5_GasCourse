4cd5539bb8d18bd61eeaca30188761e24a332e4b
Joel Keeling <7408775+joelkeeling@users.noreply.github.com>
2025-04-07 22:04:42 -0600

    7.55

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,5 +11,12 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                 const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, "EffectApplied");
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (auto& Tag : TagContainer)
+	{
+		// TODO: Broadcast the tag to widget controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
